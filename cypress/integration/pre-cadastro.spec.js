16ce3fcb934fca6b07e3b1f4e7f10305caa5bffd
Geovanna Pinheiro <geovanna.pinheiro@hotmail.com>
2022-02-16 16:19:51 -0300

    Feature Pré-cadastro com variáveis

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -8,15 +8,20 @@ describe('Funcionalidade Pré-cadastro', () => {
 
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        //Setting faker variables
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let email = faker.internet.email(nomeFaker) //creates an email address matching the first faker name
+
+
+        //Filling out form
+        cy.get('#reg_email').type(email)
         cy.get('#reg_password').type('!teste@teste$')
         cy.get(':nth-child(4) > .button').click()
-
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type('Geovanna')
-        cy.get('#account_last_name').type('Pinheiro')
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()      
-
 
     });
 
